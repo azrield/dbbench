9cb2b6d187c6f6a6af0d6bdaaa1d591e51d99f49
Dany Azriel <danya@infinidat.com>
2024-11-26 17:27:44 +0200

    added tkrzw wrapper, updated Makefile

--- Makefile
+++ Makefile
@@ -4,19 +4,17 @@ MAINSRCS = main.c args.c histogram.c random.c
 MAINOBJS = main.o args.o histogram.o random.o
 
 TESTSRCS = t_bdb.c t_lmdb.c t_leveldb.cc t_basho.cc t_hyper.cc t_rocksdb.cc \
-           t_pebbles.cc
+           t_pebbles.cc t_tkrzw.cc
 TESTOBJS = t_bdb.o t_lmdb.o t_leveldb.o t_basho.o t_hyper.o t_rocksdb.o \
-           t_pebbles.o
+           t_pebbles.o t_tkrzw.o
 
 BINDIR = bin
-TESTS = $(BINDIR)/t_bdb $(BINDIR)/t_lmdb $(BINDIR)/t_leveldb \
-	$(BINDIR)/t_basho $(BINDIR)/t_hyper $(BINDIR)/t_rocksdb $(BINDIR)/t_badger \
-        $(BINDIR)/t_pebbles
+TESTS = $(BINDIR)/t_tkrzw
 
 OPT = -O2 -DNDEBUG
 CC = gcc -pthread
 CXX = g++ -pthread
-CFLAGS = $(OPT)
+CFLAGS = $(OPT) -I/usr/local/include/tkrzw/
 
 all: $(BINDIR) $(TESTS)
 
@@ -29,39 +27,44 @@ clean:
 dbb.o:	$(MAINOBJS)
 	$(LD) -r -o $@ $^
 
-$(BINDIR)/t_bdb: dbb.o t_bdb.o
-	$(CC) -o $@ $^ -Wl,-Bstatic -ldb -Wl,-Bdynamic -lm
+#$(BINDIR)/t_bdb: dbb.o t_bdb.o
+#	$(CC) -o $@ $^ -Wl,-Bstatic -ldb -Wl,-Bdynamic -lm
 
 $(BINDIR)/t_lmdb: dbb.o t_lmdb.o
 	$(CC) -o $@ $^ -Wl,-Bstatic -llmdb -Wl,-Bdynamic -lsnappy -lm
 
-$(BINDIR)/t_mdbx: dbb.o t_mdbx.o
-	$(CC) -o $@ $^ -Wl,-Bstatic -lmdbx -Wl,-Bdynamic -lsnappy -lm
+#$(BINDIR)/t_mdbx: dbb.o t_mdbx.o
+#	$(CC) -o $@ $^ -Wl,-Bstatic -lmdbx -Wl,-Bdynamic -lsnappy -lm
+#
+#$(BINDIR)/t_leveldb: dbb.o t_leveldb.o
+#	$(CXX) -o $@ $^ ../leveldb/out-static/libleveldb.a -lsnappy
+#t_leveldb.o: t_leveldb.cc
+#	$(CXX) -c $(CFLAGS) -I../leveldb/include $^
+#
+#$(BINDIR)/t_basho: dbb.o t_basho.o
+#	$(CXX) -o $@ $^ ../basho_leveldb/libleveldb.a -lsnappy
+#t_basho.o: t_basho.cc
+#	$(CXX) -c $(CFLAGS) -I../basho_leveldb/include $^
+#
+#$(BINDIR)/t_hyper: dbb.o t_hyper.o
+#	$(CXX) -o $@ $^ ../HyperLevelDB/.libs/libhyperleveldb.a -lsnappy
+#t_hyper.o: t_hyper.cc
+#	$(CXX) -c $(CFLAGS) -I../HyperLevelDB/include $^
+#
+#$(BINDIR)/t_pebbles: dbb.o t_pebbles.o
+#	$(CXX) -o $@ $^ ../pebblesdb/.libs/libhyperleveldb.a -lsnappy
+#t_pebbles.o: t_pebbles.cc
+#	$(CXX) -c $(CFLAGS) -I../pebblesdb/include $^
 
-$(BINDIR)/t_leveldb: dbb.o t_leveldb.o
-	$(CXX) -o $@ $^ ../leveldb/out-static/libleveldb.a -lsnappy
-t_leveldb.o: t_leveldb.cc
-	$(CXX) -c $(CFLAGS) -I../leveldb/include $^
+#$(BINDIR)/t_rocksdb: dbb.o t_rocksdb.o
+#	$(CXX) -o $@ $^ ../rocksdb/librocksdb.a -ldl -lsnappy -lz 
+#t_rocksdb.o: t_rocksdb.cc
+#	$(CXX) -std=c++11 -c $(CFLAGS) -I../rocksdb/include $^
 
-$(BINDIR)/t_basho: dbb.o t_basho.o
-	$(CXX) -o $@ $^ ../basho_leveldb/libleveldb.a -lsnappy
-t_basho.o: t_basho.cc
-	$(CXX) -c $(CFLAGS) -I../basho_leveldb/include $^
-
-$(BINDIR)/t_hyper: dbb.o t_hyper.o
-	$(CXX) -o $@ $^ ../HyperLevelDB/.libs/libhyperleveldb.a -lsnappy
-t_hyper.o: t_hyper.cc
-	$(CXX) -c $(CFLAGS) -I../HyperLevelDB/include $^
-
-$(BINDIR)/t_pebbles: dbb.o t_pebbles.o
-	$(CXX) -o $@ $^ ../pebblesdb/.libs/libhyperleveldb.a -lsnappy
-t_pebbles.o: t_pebbles.cc
-	$(CXX) -c $(CFLAGS) -I../pebblesdb/include $^
-
-$(BINDIR)/t_rocksdb: dbb.o t_rocksdb.o
-	$(CXX) -o $@ $^ ../rocksdb/librocksdb.a -ldl -lsnappy -llz4 -lz -lbz2 -lzstd
-t_rocksdb.o: t_rocksdb.cc
-	$(CXX) -std=c++11 -c $(CFLAGS) -I../rocksdb/include $^
+$(BINDIR)/t_tkrzw: dbb.o t_tkrzw.o
+	$(CXX) -o $@ $^ ../tkrzw/tkrzw-1.0.32/libtkrzw.a -ldl -lsnappy -lz
+t_tkrzw.o: t_tkrzw.cc
+	$(CXX) -std=c++17 -c $(CFLAGS) -I/usr/local/include $^
 
 $(BINDIR)/t_badger: t_badger/t_badger
 	cp t_badger/t_badger $(BINDIR)
---
+++ t_tkrzw.cc
@@ -0,0 +1,157 @@
+/******************************************************************************************************/
+/*                                                                                                    */
+/*    INFINIDAT LTD. - Proprietary and Confidential Material                                          */
+/*                                                                                                    */
+/*    Copyright (C) 2024, Infinidat Ltd. - All Rights Reserved                                     */
+/*                                                                                                    */
+/*    NOTICE: All information contained herein is, and remains the property                           */
+/*    of Infinidat Ltd. All information contained herein is protected by                              */
+/*    trade secret or copyright law. The intellectual and technical concepts                          */
+/*    contained herein are proprietary to Infinidat Ltd., and may be                                  */
+/*    protected by U.S. and foreign patents, or patents in progress.                                  */
+/*                                                                                                    */
+/*    Redistribution or use, in source or binary forms, with or without                               */
+/*    modification, are strictly forbidden unless prior written permission                            */
+/*    is obtained from Infinidat Ltd.                                                                 */
+/*                                                                                                    */
+/******************************************************************************************************/
+
+#include <tkrzw_dbm.h>
+#include <tkrzw_dbm_hash.h>
+#include <tkrzw_dbm_tree.h>
+#include <tkrzw_dbm_skip.h>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+
+#include "dbb.h"
+
+using namespace tkrzw;
+
+// Configuration flags for Tkrzw options
+static int FLAGS_write_buffer_size = 0;
+static int FLAGS_max_file_size = 0;
+static int FLAGS_cache_size = -1;
+static int FLAGS_open_files = 0;
+
+static DBM* db = nullptr;
+
+static void db_open(int dbflags) {
+    HashDBM* hash_db = new HashDBM();  // Change this to TreeDBM or SkipDBM if needed
+
+    HashDBM::TuningParameters params;
+    params.update_mode = HashDBM::UPDATE_IN_PLACE;
+    params.cache_buckets = FLAGS_cache_size > 0 ? FLAGS_cache_size : -1;
+
+    Status s = hash_db->OpenAdvanced(FLAGS_db, true, File::OPEN_TRUNCATE, params).OrDie();
+    if (!s.IsOK()) {
+        fprintf(stderr, "Open error: %s\n", s.GetMessage().c_str());
+        delete hash_db;
+        exit(1);
+    }
+    db = hash_db;
+}
+
+static void db_close() {
+    if (db) {
+        Status s = db->Close();
+        if (!s.IsOK()) {
+            fprintf(stderr, "Close error: %s\n", s.GetMessage().c_str());
+        }
+        delete db;
+        db = nullptr;
+    }
+}
+
+static void db_write(DBB_local *dl) {
+    DBB_global *dg = dl->dl_global;
+
+    DBB_val dv;
+    dv.dv_size = FLAGS_value_size;
+    int64_t bytes = 0;
+    unsigned long i = 0;
+
+    do {
+        for (int j = 0; j < dg->dg_batchsize; j++) {
+            const uint64_t k = (dg->dg_order == DO_FORWARD) ? i + j : (DBB_random(dl->dl_rndctx) % FLAGS_num);
+            char key[100];
+            snprintf(key, sizeof(key), "%016lx", k);
+
+            // Generate random value
+            DBB_randstring(dl, &dv);
+            std::string value((const char*)dv.dv_data, dv.dv_size);
+
+            // Write to Tkrzw database
+            Status s = db->Set(key, value);
+            if (!s.IsOK()) {
+                fprintf(stderr, "Write error: %s\n", s.GetMessage().c_str());
+                exit(1);
+            }
+            bytes += FLAGS_value_size + FLAGS_key_size;
+            DBB_opdone(dl);
+        }
+        i += dg->dg_batchsize;
+    } while (!DBB_done(dl));
+    dl->dl_bytes += bytes;
+}
+
+static void db_read(DBB_local *dl) {
+    DBB_global *dg = dl->dl_global;
+
+    int64_t bytes = 0;
+    size_t found = 0;
+    std::string value;
+    char key[100];
+
+    do {
+        const uint64_t k = DBB_random(dl->dl_rndctx) % FLAGS_num;
+        snprintf(key, sizeof(key), "%016lx", k);
+
+        Status s = db->Get(key, &value);
+        if (s.IsOK()) {
+            bytes += FLAGS_key_size + value.size();
+            found++;
+        } else if (s == Status::NOT_FOUND_ERROR) {
+            fprintf(stderr, "Read error: %s\n", s.GetMessage().c_str());
+            exit(1);
+        }
+        DBB_opdone(dl);
+    } while (!DBB_done(dl));
+    dl->dl_bytes += bytes;
+
+    char msg[100];
+    snprintf(msg, sizeof(msg), "(%zd found)", found);
+    DBB_message(dl, msg);
+}
+
+static char *db_verstr() {
+    static char vstr[32];
+    snprintf(vstr, sizeof(vstr), "%s", "Tkrzw 1.0");  // Replace with actual Tkrzw version if available
+    return vstr;
+}
+
+// Option descriptions for the benchmark tool
+static arg_desc db_opts[] = {
+    { "write_buffer_size", arg_int, &FLAGS_write_buffer_size },
+    { "max_file_size", arg_int, &FLAGS_max_file_size },
+    { "cache_size", arg_int, &FLAGS_cache_size },
+    { "open_files", arg_int, &FLAGS_open_files },
+    { NULL }
+};
+
+// Define Tkrzw backend for DBBench
+static DBB_backend db_tkrzw = {
+    "tkrzw",
+    "Tkrzw",
+    db_opts,
+    db_verstr,
+    db_open,
+    db_close,
+    db_read,
+    db_write
+};
+
+extern DBB_backend *dbb_backend;
+DBB_backend *dbb_backend = &db_tkrzw;
+
+
